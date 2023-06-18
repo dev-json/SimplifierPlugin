80caba883244ffd3a111c182170c8abe40f6db12
_JXc <hsvbuh12@gmail.com>
2023-06-18 17:51:18 +0200

    Initial Upload

---
+++ src/main/java/de/jxson/simplifier/SimplifierPlugin.java
@@ -0,0 +1,16 @@
+package de.jxson.simplifier;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SimplifierPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
